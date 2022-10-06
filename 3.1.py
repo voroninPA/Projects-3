4f97287434f9893bf0dbea8ba13809951930debf
voroninPA <114736747+voroninPA@users.noreply.github.com>
2022-10-06 16:53:43 +0300

    Create 3.1.py

---
+++ 3.1.py
@@ -0,0 +1,10 @@
+b=int(input("Введите величину угла:"))
+a=b%360
+pi=3.141592653589
+x=a*pi/180
+coss=1-x**2/2+x**4/24
+sinn=x-x**3/6+x**5/120
+tann=sinn/coss
+print("sin= ", "%.3f" %sinn)
+print("cos= ", "%.3f" %coss)
+print("tan= ", "%.3f" %tann)
